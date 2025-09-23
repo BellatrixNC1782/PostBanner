7c9034acc9fe32434a97c5b1d4435b27616f9bb2
Ashok Gosai <ashok.goswami@octosglobal.info>
2025-09-23 15:13:30 +0530

    terms & privacy changes

--- resources/views/web/privacypolicy/privacypolicymobile.blade.php
+++ resources/views/web/privacypolicy/privacypolicymobile.blade.php
@@ -1,1 +1,49 @@
-{!! $privacypolicy->document_file !!}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title> Build in Book </title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=7" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" href="{{ asset('public/images/favicons.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('public/web/css/bootstrap.min.css') }}" />
+
+    <link href="{{ asset('public/web/css/style.css') }}" type="text/css" rel="stylesheet" />
+    <link href="{{ asset('public/web/css/responsive.css') }}" type="text/css" rel="stylesheet" />
+    <link href="{{ asset('public/web/css/alertify.min.css') }}" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+</head>
+
+<body>
+    <!------------------------news details section start----------------------------->
+    <section class="privacy-policy content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-xs-12">
+                    {!! @$privacypolicy->document_file !!}
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!---------------------------End---------------------------------->
+    <script src="{{ asset('public/web/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('public/web/js/popper.min.js') }}"></script>
+    <script src="{{ asset('public/web/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('public/web/js/alertify.min.js') }}"></script>
+    <script src="{{ asset('public/web/js/validate.js') }}"></script>
+
+    <script src="{{ asset('public/web/js/script.js') }}"></script>
+
+
+    <!-- Swiper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/js/swiper.min.js "></script>
+
+</body>
+
+</html>
